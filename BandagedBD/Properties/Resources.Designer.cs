2cf46226ae7e0d15a157367a66003b5fb544f046
Zack Rauen <rauenzi@outlook.com>
2020-07-29 16:07:25 -0400

    Bump version

--- BandagedBD/Properties/Resources.Designer.cs
+++ BandagedBD/Properties/Resources.Designer.cs
@@ -114,7 +114,7 @@ namespace BandagedBD.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.4.
+        ///   Looks up a localized string similar to 1.0.5.
         /// </summary>
         internal static string Version {
             get {
